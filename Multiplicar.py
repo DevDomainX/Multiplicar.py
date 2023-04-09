fc0337d5c4d945d9c041cf7d43b4c0cc8abd9541
HansSaldias <126526223+HansSaldias@users.noreply.github.com>
2023-04-09 01:03:02 -0400

    scrip solo para multiplicar dos numeros
    *Herramienta comun en scrip*

---
+++ Multiplicar.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#Author: Hacker NoDo
+from colorama import init, Fore, Back, Style
+print(Fore.RED+'''
+8b    d8 88   88 88     888888 88
+88b  d88 88   88 88       88   88
+88YbdP88 Y8   8P 88  .o   88   88
+88 YY 88 `YbodP' 88ood8   88   88 oooooooooo
+''')
+
+def Multiplicar(a, b):
+	return a*b
+
+a = int(input(Fore.GREEN+)"\nIngresa un valor: "))
+b= int(input("\nIngresa segundo valor: "))
+print(Fore.YELLOW+"\nResultado: ",Multiplicar(a, b))
+print(Fore.BLUE+"\nSubscribete a mi canal: https://youtube.com/@hackerNoDo")
